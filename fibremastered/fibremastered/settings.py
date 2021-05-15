c263b4356ebe8689476dade5881625393c1ec908
Mario <ich.sphon@gmail.com>
2021-05-15 12:52:39 +0200

    Add static stuff

--- fibremastered/fibremastered/settings.py
+++ fibremastered/fibremastered/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATICFILES_DIRS= [os.path.join(BASE_DIR,'static')]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -39,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'fibremastered',
     'sitetree',
+    'bulma',
 ]
 
 MIDDLEWARE = [
